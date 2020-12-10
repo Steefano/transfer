ff0573c3f53c5d13d3588e2500856aa92a3e0c0a
Steefano <56517449+Steefano@users.noreply.github.com>
2020-12-10 21:56:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from setuptools import setup
+
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(
     name = "transfer",
     author = "Stefano Rando",
     url = "git@github.com/Steefano/transfer",
     author_email = "ste.rando.97@gmail.com",
-    packages = setuptools.find_packages(),
+    packages = find_packages(),
     install_requires = ["numpy", "scikit-learn", "cvxpy"],
     version = "0.0.1"
     license = "MIT",
-    description = "A package containing methods and tools for transfer learning."
+    description = "A package containing methods and tools for transfer learning.",
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    classifiers = [
+        "Programming language :: Python :: 3"
+    ]
 )
