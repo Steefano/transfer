bba65169e8f3151a7c26364be8b6dee8aa4683bc
Steefano <56517449+Steefano@users.noreply.github.com>
2020-12-10 22:01:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email = "ste.rando.97@gmail.com",
     packages = find_packages(),
     install_requires = ["numpy", "scikit-learn", "cvxpy"],
-    version = "0.0.1"
+    version = "0.0.1",
     license = "MIT",
     description = "A package containing methods and tools for transfer learning.",
     long_description = README,
