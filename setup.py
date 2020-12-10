efbe1f16c4b9fe6eb0e6e9abfd2da6fdb829002c
Steefano <56517449+Steefano@users.noreply.github.com>
2020-12-10 21:26:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "transfer",
     author = "Stefano Rando",
-    url = "https://github.com/Steefano/transfer",
+    url = "git@github.com/Steefano/transfer",
     author_email = "ste.rando.97@gmail.com",
     packages = setuptools.find_packages(),
     install_requires = ["numpy", "scikit-learn", "cvxpy"],
