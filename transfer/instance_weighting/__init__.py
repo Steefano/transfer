ec81dafe2fb72a02ae25680f000497df97f65396
Steefano <56517449+Steefano@users.noreply.github.com>
2020-12-10 22:07:12 +0100

    Update __init__.py

--- transfer/instance_weighting/__init__.py
+++ transfer/instance_weighting/__init__.py
@@ -1,3 +1,5 @@
 from ._classes import KernelMeanMatching
 from ._classes import KullbackLeiblerImportance
 from ._classes import TwoStageWeighting
+
+__all__ = ["KernelMeanMatching", "KullbackLeiblerImportance", "TwoStageWeighting"]
