0dcd084058e3aeb1b4cb6487c15c29884e6e0c16
Steefano <56517449+Steefano@users.noreply.github.com>
2020-12-10 21:09:53 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "transfer",
+    author = "Stefano Rando",
+    author_email = "ste.rando.97@gmail.com",
+    packages = setuptools.find_packages(),
+    install_requires = ["numpy", "scikit-learn", "cvxpy"],
+    version = "0.0.1"
+    license = "MIT",
+    description = "A package containing methods and tools for transfer learning."
+)
