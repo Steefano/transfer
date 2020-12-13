6bf2ba8fa34ebf83a17d023f6ec6640ff29efeca
Stefano <56517449+Steefano@users.noreply.github.com>
2020-12-13 23:38:29 +0100

    Update _classes.py

--- transfer/feature_transformation/_classes.py
+++ transfer/feature_transformation/_classes.py
@@ -10,6 +10,28 @@ import numpy as np
 class FeatureReplication:
   """
   Feature augmentation of the space through features' replication (FDA).
+  
+  FDA works by triplicating the feature space so that every new point, in the new space, contains
+  features referring to the source domain, features referring to the target domain and
+  features referring to both.
+  
+  More specifically, the two transformations
+  .. math::
+	
+	  \begin{eqnarray}
+	  \phi_{1} : X_{S} \longrightarrow \bar{X} \\
+	  \phi_{2} : X_{T} \longrightarrow \bar{X}
+	  \end{eqnarray}
+    
+  are defined as
+  .. math::
+	
+	  \begin{eqnarray}
+	  \phi_{1}(x) = \left[x, x, 0right] \\
+	  \phi_{2}(x) = \left[x, 0, x\right]
+	  \end{eqnarray}
+    
+  The generalization to multiple source domains is straightforward.
   """
 
   def __init__(self):
@@ -17,22 +39,23 @@ class FeatureReplication:
 
   def fit(self, Xs, Xt):
     """
-    Fits the model with Xs and Xt.
+    Fits the model with :math:`X_{S}` and :math:`X_{T}`.
 
     Parameters
     ----------
 
     Xs : list of arrays
-      list of data points from the source domains
+      List of arrays, each one containing points from a specific
+      source domain.
     
     Xt : array of shape (n_samples_t, n_features)
-      target domain data points
+      Array containing points from the target domain.
 
     Returns
     -------
 
-    self
-      fitted model
+    FeatureReplication
+      Fitted model
     """
 
     self._k = len(Xs)
@@ -42,22 +65,23 @@ class FeatureReplication:
 
   def transform(self, Xs, Xt):
     """
-    Apply the model to source domains' point Xs and target domain points Xt.
+    Applies the model to source domains' point :math:`X_{S}` and target domain points :math:`X_{T}.
 
     Parameters
     ----------
 
     Xs : list of arrays
-      list of data points from the source domains
+      List of arrays, each one containing points from a specific
+      source domain.
 
     Xt : array of shape (n_samples_t, n_features)
-      target domain data points
+      Array containing points from the target domain.
 
     Returns
     -------
 
-    array of shape (product(Xs.shape), self._n_features)
-      data points in the new space of replicated features
+    array
+      Array of transformed data points.
     """
 
     to_return = np.array([np.zeros(self._n_features * (self._k + 2))])
@@ -73,5 +97,25 @@ class FeatureReplication:
     return to_return[1:]
 
   def fit_transform(self, Xs, Xt):
+    """
+    Fits the model with :math:`X_{S}` and :math:`X_{T}` and applies it.
+    It returns the transformed points.
+
+    Parameters
+    ----------
+
+    Xs : list of arrays
+      List of arrays, each one containing points from a specific
+      source domain.
+
+    Xt : array of shape (n_samples_t, n_features)
+      Array containing points from the target domain.
+
+    Returns
+    -------
+
+    array
+      Array of transformed data points.
+    """
     self.fit(Xs, Xt)
     return self.transform(Xs, Xt)
